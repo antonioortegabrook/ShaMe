06a83443a42fd9a9b1d07d1eaf1765b8cf42c347
Antonio Ortega Brook <antonioortegabrook@me.com>
2017-11-11 16:40:33 -0300

    Update Bundle Identifier

--- sha_me-plugins/ShaMe_send/JuceLibraryCode/AppConfig.h
+++ sha_me-plugins/ShaMe_send/JuceLibraryCode/AppConfig.h
@@ -357,7 +357,7 @@
  #define JucePlugin_AUManufacturerCode     JucePlugin_ManufacturerCode
 #endif
 #ifndef  JucePlugin_CFBundleIdentifier
- #define JucePlugin_CFBundleIdentifier     com.ortegabrook.ShaMe_send
+ #define JucePlugin_CFBundleIdentifier     com.ortegabrook.ShaMe.send
 #endif
 #ifndef  JucePlugin_RTASCategory
  #define JucePlugin_RTASCategory           ePlugInCategory_None
