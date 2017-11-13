f58f3780a2fc888fb6e31574a89f85bc7981697b
Antonio Ortega Brook <antonioortegabrook@me.com>
2017-11-13 12:10:27 -0300

    fixed indentation

--- sha_me-externals/source/shame_externals/shame.receive~/shame.receive~.c
+++ sha_me-externals/source/shame_externals/shame.receive~/shame.receive~.c
@@ -325,37 +325,37 @@ void shame_receive_tilde_refresh(t_shame_receive_tilde *x)
 
 void set_readable(t_shame_receive_tilde *x, int be_verbose)
 {
-	int previous_state = x->shame_readable;
+    int previous_state = x->shame_readable;
+    
+    int match_sample_rate;
+    int match_vector_size;
+    
+    x->shame_read->reader_sample_rate[x->reader_n] = sys_getsr();
+    if (x->shame_read->sample_rate != sys_getsr()) {
 	
-	int match_sample_rate;
-	int match_vector_size;
+	match_sample_rate = false;
 	
-	x->shame_read->reader_sample_rate[x->reader_n] = sys_getsr();
-	if (x->shame_read->sample_rate != sys_getsr()) {
-		
-		match_sample_rate = false;
-		
-		if (match_sample_rate != previous_state || be_verbose)
-			object_warn((t_object *)x, "%s doesn't match sample rate", x->name.s_name);
-		
-	} else
-		match_sample_rate = true;
+	if (match_sample_rate != previous_state || be_verbose)
+	    object_warn((t_object *)x, "%s doesn't match sample rate", x->name.s_name);
 	
+    } else
+	match_sample_rate = true;
+    
+    
+    x->shame_read->reader_vector_size[x->reader_n] = sys_getblksize();
+    if (x->shame_read->vector_size != sys_getblksize()) {
 	
-	x->shame_read->reader_vector_size[x->reader_n] = sys_getblksize();
-	if (x->shame_read->vector_size != sys_getblksize()) {
-		
-		match_vector_size = false;
-		
-		if (match_vector_size != previous_state || be_verbose)
-			object_warn((t_object *)x, "%s doesn't match vector size", x->name.s_name);
-		
-	} else
-		match_vector_size = true;
+	match_vector_size = false;
 	
+	if (match_vector_size != previous_state || be_verbose)
+	    object_warn((t_object *)x, "%s doesn't match vector size", x->name.s_name);
 	
-	x->shame_readable = (match_sample_rate && match_vector_size);
-	
-	if (x->shame_readable && (!previous_state || be_verbose))
-		object_post((t_object *)x, "%s is readable", x->name.s_name);
+    } else
+	match_vector_size = true;
+    
+    
+    x->shame_readable = (match_sample_rate && match_vector_size);
+    
+    if (x->shame_readable && (!previous_state || be_verbose))
+	object_post((t_object *)x, "%s is readable", x->name.s_name);
 }
